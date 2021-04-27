ae9e1949bcc0fc32f78e7c5281e26fab9f95adc4
Mikhail Rakul <micronX@yandex.ru>
2021-04-27 21:27:29 +0300

    SELECT.sql: #18 Book is added

--- SQLite_requests/SELECT.sql
+++ SQLite_requests/SELECT.sql
@@ -4,34 +4,35 @@
 PRAGMA foreign_keys = 1;
 
 -- SELECE basics: LIKE
--- SELECT cust_id, cust_name
--- FROM Customers
--- WHERE cust_name LIKE "%Toy%";
+    -- SELECT cust_id, cust_name
+    -- FROM Customers
+    -- WHERE cust_name LIKE "%Toy%";
 
 --  GLOB:
--- SELECT * FROM Customers
--- WHERE cust_state GLOB '[^AM]*' -- not A or M letter
--- /* WHERE cust_email IS NULL -- not A or M letter */
--- ORDER BY cust_contact LIMIT 5;
+    -- SELECT * FROM Customers
+    -- WHERE cust_state GLOB '[^AM]*' -- not A or M letter
+    -- /* WHERE cust_email IS NULL -- not A or M letter */
+    -- ORDER BY cust_contact LIMIT 5;
 
 -- Concatenation, RTRIM
--- SELECT RTRIM(LTRIM(vend_id || ' ' || vend_name || '         '))
--- AS INFORMATION FROM Vendors;
+    -- SELECT RTRIM(LTRIM(vend_id || ' ' || vend_name || '         '))
+    -- AS INFORMATION FROM Vendors;
 
 -- INNER JOIN
--- SELECT OrderItems.order_num, order_item, prod_id, quantity, Orders.cust_id FROM OrderItems
--- INNER JOIN Orders
--- ON OrderItems.order_num = Orders.order_num;
+    -- SELECT OrderItems.order_num, order_item, prod_id, quantity, Orders.cust_id FROM OrderItems
+    -- INNER JOIN Orders
+    -- ON OrderItems.order_num = Orders.order_num;
 
 -- Calculated field
--- SELECT DISTINCT order_num, -- order_item, quantity, item_price, --  prod_id, quantity, item_price,
--- quantity * item_price AS total_item_price
--- FROM OrderItems
--- WHERE order_num = 20008;
+    -- SELECT DISTINCT order_num, -- order_item, quantity, item_price, --  prod_id, quantity, item_price,
+    -- quantity * item_price AS total_item_price
+    -- FROM OrderItems
+    -- WHERE order_num = 20008;
 
 -- FUNCTIONS:
  -- DATE() and TIME()
-  -- SELECT DATE('now') AS current_date, strftime('%H:%M:%S') /* TIME() */ AS current_time;
+    -- SELECT DATE('now') AS current_date;
+    -- SELECT strftime('%H:%M:%S') /* TIME() */ AS current_time;
  -- UPPER, LENGTH
     -- SELECT vend_name,
     --        UPPER(vend_name) AS vend_name_upcase,
@@ -40,45 +41,46 @@ PRAGMA foreign_keys = 1;
     -- FROM Vendors
     -- ORDER BY vend_name;
 
--- DATA comparing
--- SELECT *
--- FROM Orders
--- WHERE strftime('%m', order_date) = "01" AND
---       strftime('%Y', order_date) = "2020"
--- OR =>  CAST(strftime('%m', order_date) as decimal) = 01;
+-- DATE comparing
+    -- SELECT *
+    -- FROM Orders
+    -- WHERE strftime('%m', order_date) = "01" AND
+    --       strftime('%Y', order_date) = "2020";
+    -- OR =>  CAST(strftime('%m', order_date) as decimal) = 01;
 
 -- *** Data Manipulation Functions and Summarazing Functions ***
 -- 1:
--- SELECT AVG(prod_price) AS AVG_PRICE,
---        MAX(prod_price) AS MAX_PRICE,
---        MIN(prod_name) AS FIRST_PRODUCT, -- check test Products
---        COUNT(*) AS NUM_OF_PRODUCTS
--- FROM Products;
+    -- SELECT AVG(prod_price) AS AVG_PRICE,
+    --        MAX(prod_price) AS MAX_PRICE,
+    --        MIN(prod_name) AS FIRST_PRODUCT, -- check test Products
+    --        COUNT(*) AS NUM_OF_PRODUCTS
+    -- FROM Products;
 -- 2: subquery
--- SELECT *
--- FROM Products
--- WHERE prod_price = (SELECT MIN(prod_price) FROM Products);
+    -- SELECT *
+    -- FROM Products
+    -- WHERE prod_price = (SELECT MIN(prod_price) FROM Products)
+    --  OR prod_price = (SELECT MAX(prod_price) FROM Products);
 -- 3: SUM
--- SELECT SUM(quantity) AS ORDERED_NUMBER,
---        SUM(item_price * quantity) AS TOTAL_PRICE
--- FROM OrderItems
--- WHERE order_num = 20005;
+    -- SELECT SUM(quantity) AS ORDERED_NUMBER,
+    --        SUM(item_price * quantity) AS TOTAL_PRICE
+    -- FROM OrderItems
+    -- WHERE order_num = 20005;
 -- 4. SUBSTR example
--- SELECT UPPER(cust_id) AS ID,
---        UPPER(cust_name) AS NAME,
---        UPPER(cust_contact) AS CONTACT,
---        UPPER(cust_city) AS CITY,
---        UPPER(SUBSTR(cust_contact, 1, 2) || SUBSTR(cust_city, 1, 3)) AS LOGIN
--- FROM Customers
--- ORDER BY cust_id;
+    -- SELECT UPPER(cust_id) AS ID,
+    --        UPPER(cust_name) AS NAME,
+    --        UPPER(cust_contact) AS CONTACT,
+    --        UPPER(cust_city) AS CITY,
+    --        UPPER(SUBSTR(cust_contact, 1, 2) || SUBSTR(cust_city, 1, 3)) AS LOGIN
+    -- FROM Customers
+    -- ORDER BY cust_id;
 -- 4: AVG by unique values (DISTINCT keyword)
--- SELECT AVG(DISTINCT prod_price) AS AVERAGE_OF_UNIQUE_PRICES
--- FROM Products
--- WHERE vend_id = 'DLL01';
+    -- SELECT AVG(DISTINCT prod_price) AS AVERAGE_OF_UNIQUE_PRICES
+    -- FROM Products
+    -- WHERE vend_id = 'DLL01';
 
--- SELECT MAX(prod_price) AS MaxPrice
--- FROM Products
--- WHERE prod_price < 10; -- WHERE has more priority than MAX
+    -- SELECT MAX(prod_price) AS MaxPrice
+    -- FROM Products
+    -- WHERE prod_price < 10; -- WHERE has more priority than MAX
 
 -- *** GROUP BY ***
 -- 1.
@@ -105,7 +107,7 @@ PRAGMA foreign_keys = 1;
     -- FROM Products
     -- GROUP BY vend_id
     -- ORDER BY MIN_PRICE;
--- Orders having more then 200 total items
+-- Orders having more than 200 total items
     -- SELECT order_num, SUM(quantity)
     -- FROM OrderItems
     -- GROUP BY order_num
@@ -226,26 +228,17 @@ PRAGMA foreign_keys = 1;
     --        AS SELECT *
     --           FROM Customers;
 -- INSERT values from one to anoter existing(!) table
-INSERT INTO CustCopy (cust_id,
-                    cust_contact,
-                    cust_email,
-                    cust_name,
-                    cust_address,
-                    cust_city,
-                    cust_state,
-                    cust_zip,
-                    cust_country)
-        SELECT cust_id,
-                cust_contact,
-                cust_email,
-                cust_name,
-                cust_address,
-                cust_city,
-                cust_state,
-                cust_zip,
-                cust_country
-        FROM Customers;
-
+    -- INSERT INTO CustCopy (cust_id, cust_contact,
+    --                     cust_email, cust_name,
+    --                     cust_address, cust_city,
+    --                     cust_state, cust_zip,
+    --                     cust_country)
+    --         SELECT cust_id, cust_contact,
+    --                 cust_email, cust_name,
+    --                 cust_address, cust_city,
+    --                 cust_state, cust_zip,
+    --                 cust_country
+    --         FROM Customers;
 
 -- #16 Book: UPDATE, DELETE
 -- 1. Update one field
@@ -270,6 +263,8 @@ INSERT INTO CustCopy (cust_id,
     -- INNER JOIN Orders ON Customers.cust_id = Orders.cust_id;
     -- SELECT *
     -- FROM CustomersWithOrders2;
+-- Check that VIEW works:
+    -- SELECT * FROM CustomersWithOrders2;
 
 -- ### CONSTRAINTS, ON DELETE CASCADE
     -- CREATE TABLE departments
@@ -287,13 +282,15 @@ INSERT INTO CustCopy (cust_id,
     --     ON DELETE CASCADE
     -- );
     --
-    -- INSERT INTO departments (department_name) VALUES ('DEPT2');
-    -- INSERT INTO departments (department_name) VALUES ('DEPT2');
-    -- INSERT INTO employees (first_name, department_id) VALUES ('Misha', 1);
+    -- INSERT INTO departments (department_name) VALUES ('DEPT1');
+    -- INSERT INTO departments (department_name) VALUES ('DEPT4');
+    -- INSERT INTO employees (first_name, department_id) VALUES ('Misha', 6);
     -- INSERT INTO employees (first_name, department_id) VALUES ('Peta', 2);
-    -- DELETE FROM departments WHERE department_id = 1;
--- ######################################
 
+    SELECT * FROM employees;
+    -- SELECT * FROM departments;
+    -- DELETE FROM departments WHERE department_id = 2;
+    DELETE FROM employees WHERE employee_id BETWEEN 5 AND 12;
 
 
 
