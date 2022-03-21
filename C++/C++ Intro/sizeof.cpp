3512d3b9f6e00c1cb569ed1cff4ac49da0e9f63e
Mikhail Rakul <micronX@yandex.ru>
2022-03-21 15:43:14 +0300

    Sizeof slighlty changed

--- C++/C++ Intro/sizeof.cpp
+++ C++/C++ Intro/sizeof.cpp
@@ -6,8 +6,13 @@ int main(int argc, char const *argv[])
 {
     int x[10];
     int *p = x;
-    cout << "Number of elements: " <<sizeof(x) / sizeof(*x) << endl;
-    cout << sizeof(p) / sizeof(*p) << endl;
+    cout << "Array size: " << sizeof(x) << endl;
+    cout << "Size of one element: " << sizeof(*x) << endl;
+    cout << "Number of elements: " << sizeof(x) / sizeof(*x) << endl;
+
+    cout << "Size of pointer :" << sizeof(p) << endl;
     cout << "Pointer size: " << sizeof(p) << " Int size: " << sizeof(*p) << endl;
+    //How much does the size of the pointer exceed the size of the number
+    cout << "Ptr Size / Int size ratio :" << sizeof(p) / sizeof(*p) << endl;
     return 0;
 }
