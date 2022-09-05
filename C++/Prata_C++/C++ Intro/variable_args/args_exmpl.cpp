ede26cc3fad7e2af6aed32c23421e7e335033089
Mikhail Rakul <micronX@yandex.ru>
2022-09-06 01:34:08 +0300

    Variable arguments minor changes with explanations

--- C++/Prata_C++/C++ Intro/variable_args/args_exmpl.cpp
+++ C++/Prata_C++/C++ Intro/variable_args/args_exmpl.cpp
@@ -4,13 +4,13 @@
 
 using namespace std;
 
-template <typename Type>                                  // Template for single argument to stop the recursionю
-void oneArgOut(const Type& funcVariable)
+template <typename Type>                                    // Template for single argument to stop the recursion
+void oneArgOut(const Type& funcVariable)                    // (+) Need to either call this funcOut() to have one argument call, or another like oneArgOut(), but then need to implement zero-arguments function for funcOut()
 {
     cout << __PRETTY_FUNCTION__ << "Current value is: " << funcVariable << endl;
 }
 
-void funcOut(){cout << "\t### Terminating funcOut() ###";}                                       //Or the solution is to have termination function for zero arguments
+void funcOut(){cout << "\t### Terminating LAST funcOut() ###" << endl;}          //Or the solution is to have termination function for zero arguments
 
 //Variadic template to accept variadic number of arguments
 template<typename Type, typename... Args>                // (!) Note that typename... is also specifies that types are different
@@ -19,13 +19,16 @@ void funcOut(const Type& firstArg, const Args&... args)  // recursive variadic f
     // cout << __PRETTY_FUNCTION__ << endl << "Current value is: " << firstArg << endl;
     oneArgOut(firstArg);
     funcOut(args...);                                    //  Calls the function for the rest of arguments. Finally, the one-argument function funcOut is called (or zero argument, if this implementation chosen)
+    cout << "Exit from funcOut(...)" << endl;
 }
 
+
+//Initializer list function (the same type arguments). Note: initializer_list always uses copy semantics, that is no references uses
 template <class Type>
-void initListFunc(initializer_list<Type> argsList)              //Initializer list function (the same type arguments). Note: initializer_list always uses copy semantics, that is no references uses
+void initListFunc(initializer_list<Type> argsList)
 {
     for (auto curArg : argsList){
-        cout << __PRETTY_FUNCTION__ << endl << "Current value is: "<<curArg << endl;
+        cout << __PRETTY_FUNCTION__ << endl << "Current value is: "<< curArg << endl;
     }
 }
 
