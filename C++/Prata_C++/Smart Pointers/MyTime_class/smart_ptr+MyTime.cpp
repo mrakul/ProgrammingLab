b6b7b32de4e1f206c72766408f4831428ce36f78
Mikhail Rakul <micronX@yandex.ru>
2022-09-19 19:12:26 +0300

    Smart Pointers: unique_ptr() factory example added

--- C++/Prata_C++/Smart Pointers/MyTime_class/smart_ptr+MyTime.cpp
+++ C++/Prata_C++/Smart Pointers/MyTime_class/smart_ptr+MyTime.cpp
@@ -3,6 +3,7 @@
 #include "../../OOP/MyTime (OpOverload)/MyTime.h"
 
 std::shared_ptr<MyTime> timeFactory(int hours, int minutes);
+std::unique_ptr<MyTime> timeFactoryUnique(int hours, int minutes);
 
 int main(int argc, char const *argv[])
 {
@@ -27,7 +28,8 @@ int main(int argc, char const *argv[])
     //3. New MyTime obj shared_ptr
     // Create a block to observe when the destructor will be called
     {
-        shared_ptr<MyTime> myTimeObj = unique_ptr<MyTime>(new MyTime(2, 3));
+        unique_ptr<MyTime>(new MyTime(10, 10));                                     // Interesting that this creates a temporary pointer and destroys it suddenly with the related object
+        shared_ptr<MyTime> myTimeObj = unique_ptr<MyTime>(new MyTime(2, 3));        // This is possible since unique_ptr is temporary (?)
         shared_ptr<MyTime> myTimeObj2 = myTimeObj;                                  // Create another pointer, which points to the same object
         // shared_ptr<MyTime> myTimeObj2 = std::make_shared<MyTime>(*myTimeObj);    // This form creates another one object basing on the myTimeObj, so there are two different objects
 
@@ -53,17 +55,25 @@ int main(int argc, char const *argv[])
         auto newSharedPtr = timeFactory(10, 30);         // Pointer is copied from the timeFactory()
         cout << "newSharedPtr.useCount() = " << newSharedPtr.use_count() << endl;       // Here it has exclusive ownership
 
-    }                                                // Destroy existing objects here
+        auto newUniquePtr = timeFactoryUnique(20, 30);                                  // This creates unique object, compiler allows to return temporary unique_ptr here
+
+    }                                                                                    // Destroy existing objects here
 
     return 0;
-
 }
 
-// factory returns a shared_ptr pointing to a dynamically allocated object
+// Factory returns a shared_ptr pointing to a dynamically allocated object
 std::shared_ptr<MyTime> timeFactory(int hours, int minutes)
 {
     // shared_ptr will take care of deleting this memory
     return std::make_shared<MyTime>(hours, minutes);            //Constructor of MyTime is called here
     // Note: above is the same as below (?)
     // return std::shared_ptr<MyTime>(new MyTime(hours, minutes));
+}
+
+// Factory returns a unique_ptr pointing to a dynamically allocated object
+std::unique_ptr<MyTime> timeFactoryUnique(int hours, int minutes)
+{
+    // shared_ptr will take care of deleting this memory
+    return std::unique_ptr<MyTime>(new MyTime(hours, minutes));            //Constructor of MyTime is called here
 }
