7fb265c9cc59c6b1cd9f9097c76aeb4fb37c2f22
Mikhail Rakul <micronX@yandex.ru>
2023-03-23 17:17:16 +0300

    C++: smart pointers examples reworked during knowledge refreshing

---
+++ C++/Prata_C++/Smart Pointers/MyTime_class/MyTime.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include "../../OOP/MyTime (OpOverload)/MyTime.h"
+
+MyTime::MyTime() : hours(0), minutes(0)     //use initializer list
+{}
+
+MyTime::MyTime(int hrs, int mnts) : hours(hrs), minutes(mnts){
+    std::cout << "MyTime constructor(int, int) is called" << std::endl;
+}
+
+MyTime::~MyTime(){
+    std::cout << "MyTime destructor is called" << std::endl;
+}
+void MyTime::Show() const
+{
+    std::cout << "It is " << hours << " hours " << minutes << " minutes" << std::endl;
+}
+
+void MyTime::AddMin(int m)
+{
+    minutes += m;
+    hours += minutes / 60;
+    minutes %= 60;
+}
+
+void MyTime::AddHr(int h)
+{
+    hours += h;
+}
+
+void MyTime::Reset(int h, int m)
+{
+    hours = h;
+    minutes = m;
+}
+
+MyTime MyTime::operator+(const MyTime &added) const     //(!) Note that operator+() creates temporary MyTime object to return
+{
+    MyTime sum;
+    sum.minutes = minutes + added.minutes;
+    sum.hours = hours + added.hours + sum.minutes / 60;
+    sum.minutes %= 60;
+    return sum;
+}
+
+MyTime MyTime::operator*(double mult) const
+{
+    MyTime multObj;
+    long totalMins = hours * mult * 60 + minutes * mult;
+    multObj.hours = totalMins / 60;
+    multObj.minutes = totalMins % 60;
+    return multObj;
+}
+
+MyTime operator*(const double mult, const MyTime &t)    //Friend function: important (!!!) that it has access to the private members of the object
+{
+    MyTime multObj;
+    long totalminutes = t.hours * mult * 60 + t.minutes * mult;
+    multObj.hours = totalminutes / 60;
+    multObj.minutes = totalminutes % 60;
+    return multObj;
+}
+
+std::ostream &operator<<(std::ostream &os, const MyTime &t){
+    std::cout << "It is " << t.hours << " hours " << t.minutes << " minutes" << std::endl;
+    return os; //returns the reference to the object itself
+}
+
+//Conversion function: from double to MyTime type
+ MyTime::MyTime(double convert){
+     minutes = convert;
+ }
+
+ //static variable initialization
+ int MyTime::b = 0;
+ const int MyTime::a = 4;
--- C++/Prata_C++/Smart Pointers/MyTime_class/smart_ptr+MyTime.cpp
+++ C++/Prata_C++/Smart Pointers/MyTime_class/smart_ptr+MyTime.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include "../../OOP/MyTime (OpOverload)/MyTime.h"
 
-std::shared_ptr<MyTime> timeFactory(int hours, int minutes);
+std::shared_ptr<MyTime> timeFactoryShared(int hours, int minutes);
 std::unique_ptr<MyTime> timeFactoryUnique(int hours, int minutes);
 
 int main(int argc, char const *argv[])
@@ -12,61 +12,74 @@ int main(int argc, char const *argv[])
     using std::shared_ptr;
     using std::string;
     using std::cout, std::endl;
+    using std::boolalpha;
 
-    //1. New string auto_ptr (auto is deprecated now)
+    // 1. New string auto_ptr() (auto is deprecated now)
     //auto_ptr<string> ptrString {new string};    //or auto_ptr<string>(new string);
 
-    //2. New MyTime unique_ptr
-    unique_ptr<MyTime> myTimeUnique = unique_ptr<MyTime>(new MyTime(5, 30));
-    // unique_ptr<MyTime> MyTimeUnique2 = MyTimeUnique;                 // Compiler restricts this: "use of deleted function 'std::unique_ptr<_Tp, _Dp>::unique_ptr(const std::unique_ptr<_Tp, _Dp>&)"
-    myTimeUnique.reset(new MyTime(6, 45));                              // Ownership to another object, the old one is destructed, MyTime destructor is called (and the deleter is called implicitly?)
-
-    MyTime *myTimePtr = myTimeUnique.release();                        // Release the ownership, so the object should be deleted by the caller for the target pointer
-
-    delete myTimePtr;                                                   // Destruct the object
-
-    //3. New MyTime obj shared_ptr
-    // Create a block to observe when the destructor will be called
+    //  2. unique_ptr() examples: observe how unique_ptr() is destroyed after quitting the block
     {
-        unique_ptr<MyTime>(new MyTime(10, 10));                                     // Interesting that this creates a temporary pointer and destroys it suddenly with the related object
-        shared_ptr<MyTime> myTimeObj = unique_ptr<MyTime>(new MyTime(2, 3));        // This is possible since unique_ptr is temporary (?)
-        shared_ptr<MyTime> myTimeObj2 = myTimeObj;                                  // Create another pointer, which points to the same object
-        // shared_ptr<MyTime> myTimeObj2 = std::make_shared<MyTime>(*myTimeObj);    // This form creates another one object basing on the myTimeObj, so there are two different objects
+        unique_ptr<MyTime> myTimeUnique = unique_ptr<MyTime>(new MyTime(5, 30));
+        // unique_ptr<MyTime> MyTimeUnique2 = MyTimeUnique;                 // Compiler restricts this: "use of deleted function 'std::unique_ptr<_Tp, _Dp>::unique_ptr(const std::unique_ptr<_Tp, _Dp>&)"
+        myTimeUnique.reset(new MyTime(6, 45));                              // Ownership to another object, the old one is destructed, MyTime destructor is called (and the deleter is called implicitly?)
 
-        cout << "myTimeObj.useCount() = " << myTimeObj.use_count()
-            << " myTimeObj2.useCount() = " << myTimeObj2.use_count() << endl;       // For both it is observed that use_count() call gives 2 -- both have ownership
+        MyTime *myTimePtr = myTimeUnique.release();                         // Release the ownership, so the object should be deleted by the caller for the target pointer.
+                                                                            // In this case, using *myTimePtr leads to Segmentation Fault (as for auto_ptr which lost the ownership of the object)
 
-        myTimeObj.reset();           //Reset the pointer, not destroying object, since myTimeObj2 still manages it
+        delete myTimePtr;                                                   // Destruct the object explicitly
 
-        cout << "myTimeObj.useCount() = " << myTimeObj.use_count()
-            << " myTimeObj2.useCount() = " << myTimeObj2.use_count() << endl;
+        // 2.1. Example with transfering ownership by using Move Semantics
+        unique_ptr<MyTime> myTimeSource = unique_ptr<MyTime>(new MyTime(1, 50));
+        unique_ptr<MyTime> myTimeTarget;
 
-        myTimeObj.unique() ? cout << "Yes " : cout << "No ";
-        cout << (myTimeObj2.unique() ? "Yes " : "No ") << endl;     //braces makes conditional operator more prioritized (meaning operator precedence)
+        // myTimeTarget = myTimeSource;                                     // This is restricted: explicit copying not to make fake copy assignment
+        myTimeTarget = move(myTimeSource);                                  // (!) But this is ALLOWED, since myTimeSource is considered as a temporary (rvalue), so it will lose the ownership in this case as if it will be not used
 
-        myTimeObj2.reset(new MyTime(7, 45));           //Reset the pointer to manage another object, destroys the object: MyTime destructor is called (implicitly called the deletor of the old one memory?)
+    }                                                                       // Note: here, myTimeUnique doesn't own the MyTime object and points to nullptr.
 
-        cout << "myTimeObj.useCount() = " << myTimeObj.use_count()
-            << " myTimeObj2.useCount() = " << myTimeObj2.use_count() << endl;
+    // 3. shared_ptr() example pointing to MyTime object
+    {
+        unique_ptr<MyTime>(new MyTime(10, 10));                                        // Interesting that this creates a temporary pointer and destroys it suddenly with the related object
+        shared_ptr<MyTime> myTimeShared = unique_ptr<MyTime>(new MyTime(2, 3));        // This is possible since unique_ptr is temporary (?)
+        shared_ptr<MyTime> myTimeShared2 = myTimeShared;                                  // Create another pointer, which points to the same object
+        // shared_ptr<MyTime> myTimeShared2 = std::make_shared<MyTime>(*myTimeShared);    // This form creates another one object basing on the (*myTimeShared), so there are two different objects
 
-        //myTimeObj2.reset();                            // Reset the pointer, destroys the object: MyTime destructor is called
+        cout << "myTimeShared.useCount() = " << myTimeShared.use_count()
+            << " myTimeShared2.useCount() = " << myTimeShared2.use_count() << endl;       // For both it is observed that use_count() call gives 2 -- both have ownership
 
-        /*** 4. timeFactory() call: ***/
-        auto newSharedPtr = timeFactory(10, 30);         // Pointer is copied from the timeFactory()
+        myTimeShared.reset();                                                             // Reset the pointer, not destroying object, since myTimeShared2 still manages it
+
+        cout << "myTimeShared.useCount() = " << myTimeShared.use_count()                  // myTimeShared points to nullptr here, since was reset (shows 0 on count)
+            << " myTimeShared2.useCount() = " << myTimeShared2.use_count() << endl;       // myTimeShared2 shows 1, since still pointing to the shared object
+
+        cout << boolalpha << "myTimeShared unique : " << myTimeShared.unique() << endl;   // Returns False, since doesn't point to any object
+        cout << boolalpha << "myTimeShared2 unique : " << myTimeShared2.unique() << endl; // Returns True, since still points to the created object
+
+        myTimeShared2.reset(new MyTime(7, 45));                                           // Reset the pointer to manage another object, destroys the object: previous MyTime destructor is called (implicitly called the deletor of the old one memory?)
+
+        cout << "myTimeShared.useCount() = " << myTimeShared.use_count()
+            << " myTimeShared2.useCount() = " << myTimeShared2.use_count() << endl;
+
+        //myTimeShared2.reset();                                                         // Reset the pointer, destroys the object: MyTime destructor is called
+    }                                                                                    // Or destory the object after of myTimeShared2 when quitting the block
+
+    /*** 4. timeFactoryShared() call: ***/
+    {
+        auto newSharedPtr = timeFactoryShared(10, 30);                                  // Pointer is copied from the timeFactoryShared(), the local pointer is destroyed after, so newSharedPtr points uniquely to the object actually
         cout << "newSharedPtr.useCount() = " << newSharedPtr.use_count() << endl;       // Here it has exclusive ownership
 
-        auto newUniquePtr = timeFactoryUnique(20, 30);                                  // This creates unique object, compiler allows to return temporary unique_ptr here and assign by move-semantics
-
-    }                                                                                    // Destroy existing objects here
+        auto newUniquePtr = timeFactoryUnique(20, 30);                                  // This creates unique object, compiler allows to return temporary unique_ptr here and assign by Move Semantics
+        timeFactoryUnique(20, 30);                                                      // Here, since the pointer is not moved to another, an object is deleted immediately (just an example for understanding)
+    }
 
     return 0;
 }
 
 // Factory returns a shared_ptr pointing to a dynamically allocated object
-std::shared_ptr<MyTime> timeFactory(int hours, int minutes)
+std::shared_ptr<MyTime> timeFactoryShared(int hours, int minutes)
 {
     // shared_ptr will take care of deleting this memory
-    return std::make_shared<MyTime>(hours, minutes);            //Constructor of MyTime is called here
+    return std::make_shared<MyTime>(hours, minutes);
     // Note: above is the same as below (?)
     // return std::shared_ptr<MyTime>(new MyTime(hours, minutes));
 }
@@ -74,7 +87,7 @@ std::shared_ptr<MyTime> timeFactory(int hours, int minutes)
 // Factory returns a unique_ptr pointing to a dynamically allocated object
 std::unique_ptr<MyTime> timeFactoryUnique(int hours, int minutes)
 {
-    // shared_ptr will take care of deleting this memory
-    return std::make_unique<MyTime>(hours, minutes);                          //Constructor of MyTime is called here
+    // unique_ptr will take care of deleting this memory
+    return std::make_unique<MyTime>(hours, minutes);
     // return std::unique_ptr<MyTime>(new MyTime(hours, minutes));
 }
